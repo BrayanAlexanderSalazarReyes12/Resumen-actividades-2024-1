99d6894bdc7e39ad801da94508ac58d97968c5de
Brayan Alexander Salazar Reyes <bsalazarreyes88@gmail.com>
2024-06-15 11:14:54 -0500

    update Formulario_asistencia.js

--- netlify/functions/Formulario_asistencia.js
+++ netlify/functions/Formulario_asistencia.js
@@ -1,56 +1,62 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event, context) => {
-    switch (event.httpMethod) {
-        case "POST":
-            const params = JSON.parse(event.body);
-            console.log("RECIBI UNA SOLICITUD", params);
-
-            // Configurar el transporte de Nodemailer para Gmail
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: "bsalazarr@unicartagena.edu.co", // dirección de Gmail desde variables de entorno
-                    pass: "1234567890" // contraseña de Gmail o contraseña específica de la aplicación desde variables de entorno
-                }
-            });
-
-            // Opciones del correo electrónico
-            let mailOptions = {
-                from: `"Sender Name" <bsalazarr@unicartagena.edu.co>`, // dirección del remitente
-                to: params.correo, // lista de destinatarios
-                subject: `certificado de asistencia ${params.nombredelevento}`, // asunto
-                text: `Gracias por su participación en el evento este es el certificado de asistencia al evento ${params.nombredelevento}`, // cuerpo del mensaje en texto plano
-                attachments: [
-                    {
-                        filename: 'asistencia.pdf', // nombre del archivo adjunto
-                        content: params.pdfBase64, // contenido del archivo en base64
-                        encoding: 'base64' // codificación del archivo
-                    }
-                ]
-            };
-
-            try {
-                // Enviar el correo electrónico
-                let info = await transporter.sendMail(mailOptions);
-                console.log("Email sent: %s", info.messageId);
-
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ message: "Email enviado con exito" })
-                };
-            } catch (error) {
-                console.error("Error sending email: ", error);
-                return {
-                    statusCode: 500,
-                    body: JSON.stringify({ message: "Error al enviar el email", error: error.message })
-                };
-            }
-
-        default:
+    try {
+        if (event.httpMethod !== "POST") {
             return {
                 statusCode: 405,
-                body: JSON.stringify({ message: "Metodo no soportado" })
+                body: JSON.stringify({ message: "Método no soportado" })
             };
+        }
+
+        const params = JSON.parse(event.body);
+        console.log("RECIBI UNA SOLICITUD", params);
+
+        // Validar que los parámetros necesarios están presentes y son válidos
+        if (!params.correo || !params.nombredelevento || !params.pdfBase64) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Faltan parámetros requeridos o son inválidos" })
+            };
+        }
+
+        // Configurar el transporte de Nodemailer para Gmail
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "bsalazarr@unicartagena.edu.co", // dirección de Gmail desde variables de entorno
+                pass: "1234567890" // contraseña de Gmail o contraseña específica de la aplicación desde variables de entorno
+            }
+        });
+
+        // Opciones del correo electrónico
+        let mailOptions = {
+            from: `"Sender Name" <bsalazarr@unicartagena.edu.co>`, // dirección del remitente
+            to: params.correo, // lista de destinatarios
+            subject: `Certificado de asistencia ${params.nombredelevento}`, // asunto
+            text: `Gracias por su participación en el evento. Adjunto encontrará el certificado de asistencia al evento ${params.nombredelevento}.`, // cuerpo del mensaje en texto plano
+            attachments: [
+                {
+                    filename: 'asistencia.pdf', // nombre del archivo adjunto
+                    content: params.pdfBase64, // contenido del archivo en base64
+                    encoding: 'base64' // codificación del archivo
+                }
+            ]
+        };
+
+        // Enviar el correo electrónico
+        let info = await transporter.sendMail(mailOptions);
+        console.log("Email enviado: %s", info.messageId);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Email enviado con éxito" })
+        };
+    } catch (error) {
+        console.error("Error en la función Lambda:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error interno del servidor al enviar el email", error: error.message })
+        };
     }
 };
