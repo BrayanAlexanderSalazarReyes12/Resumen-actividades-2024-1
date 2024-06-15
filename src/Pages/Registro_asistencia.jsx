9a625f0cbc18579ef7d1fa87a1851a566ba91e3b
Brayan Alexander Salazar Reyes <bsalazarreyes88@gmail.com>
2024-06-15 10:26:24 -0500

    update Registro_asistencia.jsx

--- src/Pages/Registro_asistencia.jsx
+++ src/Pages/Registro_asistencia.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { db } from "../database/firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { useLocation } from "react-router-dom";
-
+import { Document, Page, Text, StyleSheet, View, pdf} from '@react-pdf/renderer';
+import { saveAs } from 'file-saver';
 function Registro_asistencia() {
     const [asistente, setAsistente] = useState({
         nombreapellido: '',
@@ -20,6 +21,33 @@ function Registro_asistencia() {
         }));
     };
 
+
+    const styles = StyleSheet.create({
+        page: {
+          flexDirection: 'column',
+          backgroundColor: '#ffffff',
+          padding: 30,
+        },
+        title: {
+          fontSize: 24,
+          textAlign: 'center',
+          marginBottom: 20,
+          fontWeight: 'bold',
+          color: '#333333',
+        },
+        section: {
+          margin: 10,
+          padding: 10,
+          flexGrow: 1,
+        },
+        text: {
+          fontSize: 16,
+          textAlign: 'center',
+          margin: 5,
+          color: '#555555',
+        },
+      });
+
     const location = useLocation();
 
     const handleRegistrarAsistencia = async (e) => {
@@ -29,6 +57,35 @@ function Registro_asistencia() {
         const seleccionado = partes[2]; // Dependiendo de tu ruta específica    
 
        try {
+        const Certificadodeasistencia = (
+            <Document>
+              <Page style={styles.page}>
+                <View style={styles.section}>
+                    <Text style={styles.title}>Certificado de asistencia al evento </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>Se certifica que el la persona asistio al evento:</Text>
+                </View>
+                <View style={styles.section}>
+                    <View style={styles.section}>
+                        <Text style={styles.text}>Nombres y apellidos: {asistente.nombreapellido}</Text>
+                        <Text style={styles.text}>Documento de identificación: {asistente.numDocumentoidentificacion}</Text>
+                        <Text style={styles.text}>Estamento al que pertenece: {asistente.Estamento}</Text>
+                        <Text style={styles.text}>Programa academico al que pertenece: {asistente.Programaacademico}</Text>
+                        <Text style={styles.text}>Correo electronico: {asistente.correo}</Text>
+                    </View>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>Gracias por su participación en el evento.</Text>
+                </View>
+              </Page>
+            </Document>
+          );
+          const asPdf = pdf();
+          asPdf.updateContainer(Certificadodeasistencia);
+          const asblob = await asPdf.toBlob();
+          saveAs(asblob, 'registro_asistencia.pdf');
+
             await addDoc(collection(db,"registros_asistente"),{
                 nombreapellido: asistente.nombreapellido,
                 numDocumentoidentificacion: asistente.numDocumentoidentificacion,
